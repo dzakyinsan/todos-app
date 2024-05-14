a034bd3265a09de50b134c212f326e9c7c0ab0be
dzaky insan <dzakyinsan@192.168.10.120>
2024-05-14 21:28:42 +0700

    add user name ad header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
-import { Layout  } from "antd";
+import { Layout } from "antd";
 
-import "./style.scss"
+import { useContext } from "react";
+import MainContext from "../../context/mainContext";
+import "./style.scss";
 
 export default function Header() {
+  const { state } = useContext(MainContext)
+
   return (
     <Layout.Header>
-        
+      {state.profile.name && (
+        <div>{`Hello, ${state.profile.name}`}</div>
+      )}
     </Layout.Header>
   );
 }
